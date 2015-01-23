fa827937f506fea270a6db9c31647d545287cab3
Justin Whitfort <s3330395@student.rmit.edu.au>
2015-01-24 01:56:49 +1100

    Initial unity stuff.

---
+++ WhenGoodAIGoBad/Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController : MonoBehaviour
+{
+    public float MoveSpeed = 5;
+
+    private Rigidbody2D _rigid;
+    private Vector2 _desiredSpeed;
+
+    protected void Awake()
+    {
+        _rigid = rigidbody2D;
+    }
+
+    public void SetDesiredSpeed(Vector2 speed)
+    {
+        _desiredSpeed = speed;
+    }
+
+    protected void FixedUpdate()
+    {
+        _rigid.velocity = _desiredSpeed;
+    }
+}
---
+++ WhenGoodAIGoBad/Assets/Scripts/Character/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using InControl;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerInput : MonoBehaviour
+{
+    private CharacterController _characterController;
+    private InputDevice _inputDevice;
+
+    protected void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    protected void Start()
+    {
+        _inputDevice = InputManager.Devices[0];        
+    }
+
+    protected void Update()
+    {
+        _characterController.SetDesiredSpeed(_inputDevice.LeftStick.Vector);
+    }
+}
